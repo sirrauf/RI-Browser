d3eada941580cf14e28e7ea6640b83ffc398a8d5
Sir Ananda Rauf <sirrauf412@gmail.com>
2025-05-17 11:09:50 +0700

    Full source code RI Browser

---
+++ RIBrowser/RIBrowser/VPNMode.Designer.vb
@@ -0,0 +1,369 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VPNMode
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(VPNMode))
+        Me.ButtonGermany = New System.Windows.Forms.Button()
+        Me.ButtonNetherlands = New System.Windows.Forms.Button()
+        Me.ButtonUS = New System.Windows.Forms.Button()
+        Me.ButtonSpain = New System.Windows.Forms.Button()
+        Me.ButtonQuit = New System.Windows.Forms.Button()
+        Me.LabelGermany = New System.Windows.Forms.Label()
+        Me.LabelNetherlands = New System.Windows.Forms.Label()
+        Me.LabelUS = New System.Windows.Forms.Label()
+        Me.LabelSpain = New System.Windows.Forms.Label()
+        Me.IPBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.IPBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.IPBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.IPBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PortBox1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.PortBox2 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PortBox3 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.PortBox4 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ButtonGermany
+        '
+        Me.ButtonGermany.BackColor = System.Drawing.Color.Transparent
+        Me.ButtonGermany.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonGermany.Image = CType(resources.GetObject("ButtonGermany.Image"), System.Drawing.Image)
+        Me.ButtonGermany.Location = New System.Drawing.Point(13, 13)
+        Me.ButtonGermany.Name = "ButtonGermany"
+        Me.ButtonGermany.Size = New System.Drawing.Size(50, 30)
+        Me.ButtonGermany.TabIndex = 0
+        Me.ButtonGermany.UseVisualStyleBackColor = False
+        '
+        'ButtonNetherlands
+        '
+        Me.ButtonNetherlands.BackColor = System.Drawing.Color.Transparent
+        Me.ButtonNetherlands.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonNetherlands.Image = CType(resources.GetObject("ButtonNetherlands.Image"), System.Drawing.Image)
+        Me.ButtonNetherlands.Location = New System.Drawing.Point(13, 54)
+        Me.ButtonNetherlands.Name = "ButtonNetherlands"
+        Me.ButtonNetherlands.Size = New System.Drawing.Size(50, 30)
+        Me.ButtonNetherlands.TabIndex = 1
+        Me.ButtonNetherlands.UseVisualStyleBackColor = False
+        '
+        'ButtonUS
+        '
+        Me.ButtonUS.BackColor = System.Drawing.Color.Transparent
+        Me.ButtonUS.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonUS.Image = CType(resources.GetObject("ButtonUS.Image"), System.Drawing.Image)
+        Me.ButtonUS.Location = New System.Drawing.Point(13, 95)
+        Me.ButtonUS.Name = "ButtonUS"
+        Me.ButtonUS.Size = New System.Drawing.Size(50, 30)
+        Me.ButtonUS.TabIndex = 2
+        Me.ButtonUS.UseVisualStyleBackColor = False
+        '
+        'ButtonSpain
+        '
+        Me.ButtonSpain.BackColor = System.Drawing.Color.Transparent
+        Me.ButtonSpain.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonSpain.Image = CType(resources.GetObject("ButtonSpain.Image"), System.Drawing.Image)
+        Me.ButtonSpain.Location = New System.Drawing.Point(13, 136)
+        Me.ButtonSpain.Name = "ButtonSpain"
+        Me.ButtonSpain.Size = New System.Drawing.Size(50, 30)
+        Me.ButtonSpain.TabIndex = 3
+        Me.ButtonSpain.UseVisualStyleBackColor = False
+        '
+        'ButtonQuit
+        '
+        Me.ButtonQuit.BackColor = System.Drawing.Color.Transparent
+        Me.ButtonQuit.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.ButtonQuit.Location = New System.Drawing.Point(13, 177)
+        Me.ButtonQuit.Name = "ButtonQuit"
+        Me.ButtonQuit.Size = New System.Drawing.Size(117, 23)
+        Me.ButtonQuit.TabIndex = 4
+        Me.ButtonQuit.Text = "Quit"
+        Me.ButtonQuit.UseVisualStyleBackColor = False
+        '
+        'LabelGermany
+        '
+        Me.LabelGermany.AutoSize = True
+        Me.LabelGermany.BackColor = System.Drawing.Color.Transparent
+        Me.LabelGermany.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.LabelGermany.Location = New System.Drawing.Point(69, 22)
+        Me.LabelGermany.Name = "LabelGermany"
+        Me.LabelGermany.Size = New System.Drawing.Size(55, 15)
+        Me.LabelGermany.TabIndex = 5
+        Me.LabelGermany.Text = "Germany"
+        '
+        'LabelNetherlands
+        '
+        Me.LabelNetherlands.AutoSize = True
+        Me.LabelNetherlands.BackColor = System.Drawing.Color.Transparent
+        Me.LabelNetherlands.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.LabelNetherlands.Location = New System.Drawing.Point(69, 62)
+        Me.LabelNetherlands.Name = "LabelNetherlands"
+        Me.LabelNetherlands.Size = New System.Drawing.Size(71, 15)
+        Me.LabelNetherlands.TabIndex = 6
+        Me.LabelNetherlands.Text = "Netherlands"
+        '
+        'LabelUS
+        '
+        Me.LabelUS.AutoSize = True
+        Me.LabelUS.BackColor = System.Drawing.Color.Transparent
+        Me.LabelUS.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.LabelUS.Location = New System.Drawing.Point(69, 103)
+        Me.LabelUS.Name = "LabelUS"
+        Me.LabelUS.Size = New System.Drawing.Size(21, 15)
+        Me.LabelUS.TabIndex = 7
+        Me.LabelUS.Text = "US"
+        '
+        'LabelSpain
+        '
+        Me.LabelSpain.AutoSize = True
+        Me.LabelSpain.BackColor = System.Drawing.Color.Transparent
+        Me.LabelSpain.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.LabelSpain.Location = New System.Drawing.Point(69, 144)
+        Me.LabelSpain.Name = "LabelSpain"
+        Me.LabelSpain.Size = New System.Drawing.Size(36, 15)
+        Me.LabelSpain.TabIndex = 8
+        Me.LabelSpain.Text = "Spain"
+        '
+        'IPBox1
+        '
+        Me.IPBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.IPBox1.Location = New System.Drawing.Point(174, 20)
+        Me.IPBox1.Name = "IPBox1"
+        Me.IPBox1.Size = New System.Drawing.Size(100, 23)
+        Me.IPBox1.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label1.Location = New System.Drawing.Point(145, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(23, 15)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "IP :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label2.Location = New System.Drawing.Point(146, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 15)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "IP :"
+        '
+        'IPBox2
+        '
+        Me.IPBox2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.IPBox2.Location = New System.Drawing.Point(175, 60)
+        Me.IPBox2.Name = "IPBox2"
+        Me.IPBox2.Size = New System.Drawing.Size(100, 23)
+        Me.IPBox2.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label3.Location = New System.Drawing.Point(146, 103)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(23, 15)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "IP :"
+        '
+        'IPBox3
+        '
+        Me.IPBox3.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.IPBox3.Location = New System.Drawing.Point(175, 101)
+        Me.IPBox3.Name = "IPBox3"
+        Me.IPBox3.Size = New System.Drawing.Size(100, 23)
+        Me.IPBox3.TabIndex = 13
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label4.Location = New System.Drawing.Point(146, 144)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(23, 15)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "IP :"
+        '
+        'IPBox4
+        '
+        Me.IPBox4.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.IPBox4.Location = New System.Drawing.Point(175, 142)
+        Me.IPBox4.Name = "IPBox4"
+        Me.IPBox4.Size = New System.Drawing.Size(100, 23)
+        Me.IPBox4.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label5.Location = New System.Drawing.Point(280, 23)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 15)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Port :"
+        '
+        'PortBox1
+        '
+        Me.PortBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.PortBox1.Location = New System.Drawing.Point(321, 20)
+        Me.PortBox1.Name = "PortBox1"
+        Me.PortBox1.Size = New System.Drawing.Size(51, 23)
+        Me.PortBox1.TabIndex = 17
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label6.Location = New System.Drawing.Point(281, 63)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(35, 15)
+        Me.Label6.TabIndex = 20
+        Me.Label6.Text = "Port :"
+        '
+        'PortBox2
+        '
+        Me.PortBox2.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.PortBox2.Location = New System.Drawing.Point(322, 60)
+        Me.PortBox2.Name = "PortBox2"
+        Me.PortBox2.Size = New System.Drawing.Size(51, 23)
+        Me.PortBox2.TabIndex = 19
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label7.Location = New System.Drawing.Point(281, 103)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(35, 15)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Text = "Port :"
+        '
+        'PortBox3
+        '
+        Me.PortBox3.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.PortBox3.Location = New System.Drawing.Point(322, 100)
+        Me.PortBox3.Name = "PortBox3"
+        Me.PortBox3.Size = New System.Drawing.Size(51, 23)
+        Me.PortBox3.TabIndex = 21
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.Transparent
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label8.Location = New System.Drawing.Point(281, 144)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(35, 15)
+        Me.Label8.TabIndex = 24
+        Me.Label8.Text = "Port :"
+        '
+        'PortBox4
+        '
+        Me.PortBox4.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.PortBox4.Location = New System.Drawing.Point(322, 141)
+        Me.PortBox4.Name = "PortBox4"
+        Me.PortBox4.Size = New System.Drawing.Size(51, 23)
+        Me.PortBox4.TabIndex = 23
+        '
+        'VPNMode
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(497, 202)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.PortBox4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.PortBox3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.PortBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PortBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.IPBox4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.IPBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.IPBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.IPBox1)
+        Me.Controls.Add(Me.LabelSpain)
+        Me.Controls.Add(Me.LabelUS)
+        Me.Controls.Add(Me.LabelNetherlands)
+        Me.Controls.Add(Me.LabelGermany)
+        Me.Controls.Add(Me.ButtonQuit)
+        Me.Controls.Add(Me.ButtonSpain)
+        Me.Controls.Add(Me.ButtonUS)
+        Me.Controls.Add(Me.ButtonNetherlands)
+        Me.Controls.Add(Me.ButtonGermany)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "VPNMode"
+        Me.Text = "VPNMode"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonGermany As Button
+    Friend WithEvents ButtonNetherlands As Button
+    Friend WithEvents ButtonUS As Button
+    Friend WithEvents ButtonSpain As Button
+    Friend WithEvents ButtonQuit As Button
+    Friend WithEvents LabelGermany As Label
+    Friend WithEvents LabelNetherlands As Label
+    Friend WithEvents LabelUS As Label
+    Friend WithEvents LabelSpain As Label
+    Friend WithEvents IPBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents IPBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents IPBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents IPBox4 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PortBox1 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PortBox2 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PortBox3 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents PortBox4 As TextBox
+End Class
---
+++ RIBrowser/RIBrowser/VPNMode.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Runtime.InteropServices
+Public Class VPNMode
+    Private Sub VPNMode_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    <Runtime.InteropServices.DllImport("wininet.dll", SetLastError:=True)>
+    Private Shared Function InternetSetOption(ByVal hInternet As IntPtr, ByVal dwOption As Integer, ByVal lpBuffer As IntPtr, ByVal lpdwBufferLength As Integer) As Boolean
+    End Function
+
+    Public Structure Struct_INTERNET_PROXY_INFO
+        Public dwAccessType As Integer
+        Public proxy As IntPtr
+        Public proxyBypass As IntPtr
+    End Structure
+
+    Private Sub UseProxy(ByVal strProxy As String)
+        Const INTERNET_OPTION_PROXY As Integer = 38
+        Const INTERNET_OPEN_TYPE_PROXY As Integer = 3
+
+        Dim struct_IPI As Struct_INTERNET_PROXY_INFO
+
+        struct_IPI.dwAccessType = INTERNET_OPEN_TYPE_PROXY
+        struct_IPI.proxy = Marshal.StringToHGlobalAnsi(strProxy)
+        struct_IPI.proxyBypass = Marshal.StringToHGlobalAnsi("local")
+
+        Dim intptrStruct As IntPtr = Marshal.AllocCoTaskMem(Marshal.SizeOf(struct_IPI))
+
+        Marshal.StructureToPtr(struct_IPI, intptrStruct, True)
+
+        Dim iReturn As Boolean = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_PROXY, intptrStruct, System.Runtime.InteropServices.Marshal.SizeOf(struct_IPI))
+    End Sub
+    Private Sub ButtonOK_Click(sender As Object, e As EventArgs) Handles ButtonQuit.Click
+        Dim confirmation
+        confirmation = MsgBox("Are you sure quit ?", vbQuestion + vbYesNo, "Confirmation")
+        If confirmation = vbYes Then
+            Close()
+        Else
+
+            Me.Show()
+        End If
+    End Sub
+
+    Private Sub ButtonGermany_Click(sender As Object, e As EventArgs) Handles ButtonGermany.Click
+        Dim confirmation
+        confirmation = MsgBox("Are you sure using ip address germany ?", vbQuestion + vbYesNo, "Confirmation")
+        If confirmation = vbYes Then
+            IPBox1.Text = "88.198.50.103"
+            PortBox1.Text = "8080"
+            UseProxy(IPBox1.Text & ":" & PortBox1.Text)
+            LabelGermany.Text = "Connected"
+
+            MessageBox.Show("Connected Germany Successfull")
+        Else
+            Dim no
+            no = vbNo
+
+
+            IPBox1.Text = "125.161.86.148"
+            PortBox1.Text = "80"
+            UseProxy(IPBox1.Text & ":" & PortBox1.Text)
+            LabelGermany.Text = "Germany"
+            MessageBox.Show("Disconnected Germany Successfull")
+
+        End If
+    End Sub
+
+
+
+End Class
